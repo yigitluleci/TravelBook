d3539a47558544262719c35ec393c6f8e0315d29
Yigit <yigit.luleciz@hotmail.com>
2021-01-29 16:21:31 +0300

    Initial commit

---
+++ app/src/main/java/com/yigitluleci/travelbook/model/Place.java
@@ -0,0 +1,19 @@
+package com.yigitluleci.travelbook.model;
+
+import java.io.Serializable;
+
+public class Place implements Serializable {
+
+    public String name;
+    public Double latitude;
+    public Double longitude;
+
+    public Place(String name, Double latitude, Double longitude){
+
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+
+    }
+
+}
